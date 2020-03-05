badc076bd40fc3d674fa54ebe9ce1a46a803b06e
77066004 <simbamatutu@buaa.edu.cn>
2020-03-05 05:41:21 +0000

    addtion target i have

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
-grep -n  'int' file | awk '{sub(":"," "); print $1 }' > result
+grep -n  '$2' $1 | awk '{sub(":"," "); print $1 }' > $3
--- src/sh_test/hello_os.sh
+++ src/sh_test/hello_os.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk 'NR==8 || NR==32 || NR==128 || NR==512 || NR==1024' file>hello_os.c
+awk 'NR==8 || NR==32 || NR==128 || NR==512 || NR==1024' $1 >$2
