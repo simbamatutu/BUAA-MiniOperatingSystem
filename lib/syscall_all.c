2d23cbcc2f0ecaf9c22cc4a65fcc039767d28a66
77066004 <simbamatutu@buaa.edu.cn>
2020-04-30 06:09:57 +0000

    lab4 intmv

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -118,8 +118,11 @@ int sys_set_pgfault_handler(int sysno, u_int envid, u_int func, u_int xstacktop)
 	// Your code here.
 	struct Env *env;
 	int ret;
-
-
+	if((ret=envid2env(envid,&env,0))){
+                return ret;
+        }
+        env->env_pgfault_handler = func;
+        env->env_xstacktop = xstacktop;
 	return 0;
 	//	panic("sys_set_pgfault_handler not implemented");
 }
@@ -298,7 +301,19 @@ int sys_set_env_status(int sysno, u_int envid, u_int status)
 	// Your code here.
 	struct Env *env;
 	int ret;
-
+	if(status!=ENV_RUNNABLE && status!=ENV_NOT_RUNNABLE && status!=ENV_FREE){
+                return -E_INVAL;
+        }
+        if(status == env->env_status){
+                return;
+        }
+        if((ret = envid2env(envid,&env,0))){
+                return ret;
+        }
+        if((env->env_status != ENV_RUNNABLE) && (status == ENV_RUNNABLE)){
+                LIST_INSERT_HEAD(env_sched_list,env,env_sched_link);
+        }
+        env->env_status = status;
 	return 0;
 	//	panic("sys_env_set_status not implemented");
 }
